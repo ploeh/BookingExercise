31d9375f3aabf2c50eedf951f3ab6ce7e43f629b
Mark Seemann <mark@ploeh.dk>
2014-07-22 18:26:32 +0200

    Added a local database with a Reservations table.

---
+++ BookingApi/DbSchema.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Reservations] (
+    [Id]       INT                NOT NULL,
+    [Date]     DATETIMEOFFSET (7) NOT NULL,
+    [Name]     NVARCHAR (50)      NOT NULL,
+    [Email]    NVARCHAR (50)      NOT NULL,
+    [Quantity] INT                NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
